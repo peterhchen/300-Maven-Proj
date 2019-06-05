1b93891c5757d30d1a5e3bdee09324ced719f4c2
peterhchen <peter.hp.chen@gmail.com>
2019-06-05 15:29:31 -0700

    Autowired Annotation
    Autowire link the XML property to the data attribute in the @component.
    We do not need the setter and getter in the @Component.

---
+++ 09_AutowiredAnnotation/08_AutowiredAnnotation/src/main/java/com/phone/appName/Tire.java
@@ -0,0 +1,23 @@
+package com.phone.appName;
+
+public class Tire {
+	private String brand;
+
+	public Tire(String brand) {
+		super();
+		this.brand = brand;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	@Override
+	public String toString() {
+		return "Tire [brand=" + brand + "]";
+	}
+}
